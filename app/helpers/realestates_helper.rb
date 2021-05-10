cf31f73774a3c675a1fe891b5f268e485dbabb36
Ioannis Kousis <bonobo@otenet.gr>
2021-05-10 20:19:17 +0300

    styled index & edit and added helper code

--- app/helpers/realestates_helper.rb
+++ app/helpers/realestates_helper.rb
@@ -1,2 +1,16 @@
 module RealestatesHelper
+    
+  def indexItem(item)
+    propertyTypes = ['', 'House', 'Shop']
+    content = tag.span(item.title, class: 'title-span')
+    content << ' '
+    content << propertyTypes[item.propertyType] + '. '
+    content << item.area + '. '
+    content << (item.levels > 1 ? 'Levels ' + item.levels.to_s + ', ' : '')
+    content << 'Bathrooms ' + item.bathrooms.to_s + '. '
+    content << item.description + '. '
+    content << tag.span(item.price.to_i.to_s(:delimited), class: 'price-span')
+    content
+  end
+
 end
