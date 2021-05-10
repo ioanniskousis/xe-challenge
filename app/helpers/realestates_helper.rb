014bdcda456a7dc4e372b6fe8b492eedcda427b8
Ioannis Kousis <bonobo@otenet.gr>
2021-05-10 20:46:51 +0300

    refactored property types collection

--- app/helpers/realestates_helper.rb
+++ app/helpers/realestates_helper.rb
@@ -1,13 +1,13 @@
 module RealestatesHelper
     
   def indexItem(item)
-    propertyTypes = ['', 'House', 'Shop']
+    propertyTypes = ['', 'Residential',  'Commercial', 'Industrial', 'Agricultural', 'Special Purpose']
     content = tag.span(item.title, class: 'title-span')
     content << ' '
     content << propertyTypes[item.propertyType] + '. '
     content << item.area + '. '
     content << (item.levels > 1 ? 'Levels ' + item.levels.to_s + ', ' : '')
-    content << 'Bathrooms ' + item.bathrooms.to_s + '. '
+    content << (item.bathrooms > 0 ? 'Bathrooms ' + item.bathrooms.to_s + '. ' : '')
     content << item.description + '. '
     content << tag.span(item.price.to_i.to_s(:delimited), class: 'price-span')
     content
